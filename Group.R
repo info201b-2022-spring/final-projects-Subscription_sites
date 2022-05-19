443d21b5f7f546469efe10b5391c6df59c4a9e6a
SkyShen77 <keyus@uw.edu>
2022-05-19 14:39:22 -0700

    Testing

--- Group.R
+++ Group.R
@@ -4,6 +4,9 @@ library("tidyr")
 
 fee_df <- read.csv("Netflix _fee.csv")
 
-each_state_fee <- fee_df %>% 
+each_state_info <- fee_df %>% 
+  mutate(total_cost = Cost.Per.Month...Standard.... + Cost.Per.Month...Basic....) %>% 
   group_by(Continent) %>% 
-  summarize(mean = mean(Cost.Per.Month...Standard....))
+  summarize(mean_cost = mean(total_cost),mean_size = mean(Total.Library.Size)) %>% 
+  arrange(-mean_cost)
+  
