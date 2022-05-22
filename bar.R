2adedf448785da73cf3d7758b95ecce1373782b7
vanshtyag1 <102994913+vanshtyag1@users.noreply.github.com>
2022-05-21 19:34:54 -0700

    Update bar.R

--- bar.R
+++ bar.R
@@ -10,7 +10,9 @@ each_year_ratings <- summarize(
   total_90_plus = length(Rotten.Tomatoes >= 90/100)
 )
 
-bar_plot <- barplot(each_year_ratings$total_90_plus, names = each_year_ratings$Year,
-                                   main = "Number of movies rated 90%+ per year (after 2015)", 
-                                   xlab = "Year", ylab = "No. movies with 90%+ rating")
+#bar_plot <- barplot(each_year_ratings$total_90_plus, names = each_year_ratings$Year,
+#                                   main = "Number of movies rated 90%+ per year (after 2015)", 
+#                                   xlab = "Year", ylab = "No. movies with 90%+ rating")
 
+#The above block of code seems to print the bar plot while simply sourcing the file in the initial lines of the .rmd file, so instead of putting it here
+#we've added this code to the .rmd file as that seems to work better.
